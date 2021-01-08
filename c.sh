6649fd99c9eb695112a93408b591572505cbc6b4
dorang2 <65272412+dorang2@users.noreply.github.com>
2021-01-08 16:52:06 +0700

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+u="y"
+while [ $u = "y" ]
+do
+   python ab1.py +6285236468370 &
+   python ab1.py +6289524264559 &
+   python ab1.py +6289676728844 &
+   python ab1.py +6282232761985 &
+   python ab1.py +13026046790 &
+   python ab1.py +6281327312092 &
+   python ab1.py +6289525035640
+
+done
